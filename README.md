3fa459295d16cb3ea47c416a7ec0da5ea057f1a3
alexBarrus <sanselchistoso9@gmail.com>
2026-09-20 16:56:03 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Biblioteca
+It's a proyect base a Library, Created By Barrutieta Alexander and Alcala Jennifer.
---
+++ src/main/java/BackEnd/Biblioteca.java
@@ -0,0 +1,16 @@
+/*Barrutiea Nulutagua Juan Alexander
+Alcala Bejarano Jennifer Paola
+*/
+
+package BackEnd;
+
+/**
+ * @author Alexander
+ */
+
+public class Biblioteca {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
